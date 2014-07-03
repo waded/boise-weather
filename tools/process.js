d7fe14adf04570764e9efe7cd1ce1a1f9da9f097
Wade Dorrell <wade@dorrells.org>
2014-07-03 17:15:55 -0600

    Add node script I use to fix Easy Weather output

---
+++ tools/process.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var split = require('split');
+
+// This is the pipe I use to turn EasyWeather.txt files
+// (http://www.proweatherstation.com/easyweather/easyweather.htm)
+// into CSV that works in Excel and other agents I'm working on
+process.stdin.resume();
+process.stdin.pipe(split()).on('data', function(line) {
+	process.stdout.write(
+		line
+			// EasyWeather defaults to tab-separated, sometimes I remember to
+			// choose comma but sometimes I don't
+			.replace(/\t/, ',')
+
+			// Expect an initial "No" column, and remove it beacuse it's not
+			// useful. It ends up resetting to 0 when freeing memory on the
+			// weather station.
+			.replace(/^No,/, '')
+			.replace(/^[0-9]*,/, '')
+
+			// EasyWeather uses dd-mm-yyyy. I prefer mm/dd/yyyy.
+			.replace(/^([0-9]*)-([0-9]*)-([0-9]*)/, '$2/$1/$3,')
+
+			.concat('\n')
+	);
+});
