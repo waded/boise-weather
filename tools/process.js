73e9fa3f041c79d6f4a272de719aa4b092e76b80
Wade Dorrell <wade@dorrells.org>
2014-07-03 18:32:43 -0600

    Remove degree symbol & remove blank lines

--- tools/process.js
+++ tools/process.js
@@ -2,16 +2,24 @@
 
 var split = require('split');
 
+process.stdin.setEncoding('binary'); // ISO-8859
+
 // This is the pipe I use to turn EasyWeather.txt files
 // (http://www.proweatherstation.com/easyweather/easyweather.htm)
 // into CSV that works in Excel and other agents I'm working on
 process.stdin.resume();
 process.stdin.pipe(split()).on('data', function(line) {
-	process.stdout.write(
-		line
+	line = line.trim();
+	if (line.length)
+	{
+		process.stdout.write(
+			line
 			// EasyWeather defaults to tab-separated, sometimes I remember to
 			// choose comma but sometimes I don't
 			.replace(/\t/, ',')
+
+			// Remove degree symbols in the header
+			.replace(/°/g, '')
 
 			// Expect an initial "No" column, and remove it beacuse it's not
 			// useful. It ends up resetting to 0 when freeing memory on the
@@ -23,5 +31,6 @@ process.stdin.pipe(split()).on('data', function(line) {
 			.replace(/^([0-9]*)-([0-9]*)-([0-9]*)/, '$2/$1/$3,')
 
 			.concat('\n')
-	);
+		);
+	};
 });
